0d86540bf0a003f67e31ba23082299e5637a03f3
Esme Chant <esmemchant@gmail.com>
2026-02-11 20:37:36 -0500

    trying again

---
+++ src/lib/state.js
@@ -0,0 +1,12 @@
+const gameState = {
+    stats: {
+        wins: 0,
+        losses: 0,
+        ties: 0,
+    },
+    over: false,
+}
+
+export default gameState;
+
+//keeps track of game progress and stats
